99ce000e86afb26877bfac6ac7a7369f217e5311
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-11-10 13:32:37 +0100

    Premier commit

--- src/com/compagnie/Character.java
+++ src/com/compagnie/Character.java
@@ -1,4 +1,47 @@
 package com.compagnie;
 
 public class Character {
+    private String name;
+    private int health = 50;
+    private int damage = 5;
+    private int speed = 3;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int takeDamage(opponentDamage) {
+        characterHealth = getHealth() - opponentDamage
+        return characterHealth
+    }
+
+    public void String displayStats() {
+
+        String characterName = getName();
+        System.out.println(characterName);
+
+        String characterHealth = toString(getHealth());
+        System.out.println(characterHealth);
+
+        String characterDamage = toString(getDamage());
+        System.out.println(characterDamage);
+
+        String characterSpeed = toString(getSpeed());
+        System.out.println(characterSpeed);
+
+    }
+
+    displayStats()
 }
