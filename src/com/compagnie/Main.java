47e43c196c74bdb8f04c267e0bb15e0afa2cfe88
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-11-10 18:03:17 +0100

    Warrior display and skill added.

--- src/com/compagnie/Main.java
+++ src/com/compagnie/Main.java
@@ -1,15 +1,11 @@
 package com.compagnie;
 
-public class Main extends Thief {
+public class Main extends Warrior {
 
     public static void main(String[] args) {
-        int opponentHealth = 50;
-        for (int i = 0; i < 10; i++) {
-            int before = opponentHealth;
-            opponentHealth = CriticalDamage(opponentHealth);
-            int after = opponentHealth;
-            System.out.println("he have now : " + opponentHealth + "hp, he loses : " + (before - after) + "hp");
-        }
+        System.out.println(damage(5));
+        displayWarrior();
+
 
     }
 }
--- src/com/compagnie/Warrior.java
+++ src/com/compagnie/Warrior.java
@@ -1,4 +1,27 @@
 package com.compagnie;
 
-public class Warrior {
+public class Warrior extends Character{
+
+    private static int defense = 2;
+    private static String archetype = "Warrior";
+
+    public static int getDefense() { return defense; }
+
+    public static String getArchetype() { return archetype; }
+
+    public static int damage(int opponentDamage) {
+        int health = getHealth();
+        int attack = opponentDamage - getDefense();
+        health = getHealth() - attack;
+        return health;
+    }
+
+    private static String toString (int attribute) { return "" + attribute; }
+
+    public static void displayWarrior() {
+        System.out.println("Class : " + getArchetype());
+        displayStats();
+        System.out.println("Defense : " + getDefense());
+    }
+
 }
