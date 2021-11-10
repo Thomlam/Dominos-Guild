6704fa9fb2a1b094c52576b28797842112b0b99c
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-11-10 16:16:04 +0100

    Add Mage and display Mage and Thief.

--- src/com/compagnie/Mage.java
+++ src/com/compagnie/Mage.java
@@ -1,4 +1,37 @@
 package com.compagnie;
 
-public class Mage {
+public class Mage extends Character{
+
+    private static String archetype = "Mage";
+    private static int magicDamage = 12;
+
+    private static int getMagicDamage() {
+        return magicDamage;
+    }
+
+    public static String getArchetype() {
+        return archetype;
+    }
+
+    public static int magicAttack(int opponentHealth) {
+        int damage = opponentHealth - (getDamage() + getMagicDamage());
+        if (getMagicDamage() % 2 == 1) {
+            magicDamage = getMagicDamage() - 1;
+        }
+        else {
+            magicDamage = getMagicDamage() / 2;
+        }
+        return damage;
+    }
+
+    private static String toString(int attribute) {
+        return "" + attribute;
+    }
+
+    public static void displayMage() {
+        System.out.println("Class : " + getArchetype());
+        displayStats();
+        System.out.println("Magic Damage : " + getMagicDamage());
+    }
+
 }
