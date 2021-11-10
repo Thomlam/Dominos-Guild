a6f23ab324d95bed64eff38604f86f730a072abc
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-11-10 16:16:08 +0100

    Add Mage and display Mage and Thief.

--- src/com/compagnie/Main.java
+++ src/com/compagnie/Main.java
@@ -3,8 +3,8 @@ package com.compagnie;
 public class Main extends Thief{
 
     public static void main(String[] args) {
-        System.out.println(Damage(10));
-        System.out.println(CriticalDamage(20));
+
+        
 
     }
 }
