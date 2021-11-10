2a17d20265640ed67ff36b2ce873f1c6b9e1fadd
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-11-10 16:16:18 +0100

    Add Mage and display Mage and Thief.

--- src/com/compagnie/Thief.java
+++ src/com/compagnie/Thief.java
@@ -1,32 +1,54 @@
 package com.compagnie;
 
 public class Thief extends Character{
+    private static String archetype = "Thief";
     private static int dodge = 25;
     private static int criticalHit = 25;
 
-    public static int getDodge() {
+    public static String getArchetype() { return archetype; }
+
+    private static int getDodge() {
         return dodge;
     }
 
-    public static int getCriticalHit() {
+    private static int getCriticalHit() {
         return criticalHit;
     }
 
     public static int Damage(int opponentDamage) {
+
         if (Math.random() * 100 > getDodge()) {
             return getHealth();
         }
+
         else {
             return getHealth() - opponentDamage;
         }
     }
 
-    public static int CriticalDamage(int opponentHealth) {
-        if(Math.random() * 100 < getCriticalHit()) {
+
+    boolean crit=false;
+
+    public int CriticalDamage(int opponentHealth) {
+        
+        if((Math.random() * 100 < getCriticalHit()) && crit == false) {
             return opponentHealth - (getDamage() * 2);
+            //crit = true;
+            
         }
         else {
             return opponentHealth - getDamage();
+            //crit = false;
         }
+        
     }
+
+    public static void displayThief() {
+        System.out.println("Class : " + getArchetype());
+        displayStats();
+        System.out.println("Critical Rate : " + toString(getCriticalHit()));
+        System.out.println("Dodge Rate : " + toString(getDodge()));
+    }
+
+    private static String toString(int attribute) { return "" + attribute; }
 }
